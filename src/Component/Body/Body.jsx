1e022fde75b4f44aee2f4415c359f81240129d87
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-12-02 19:28:36 +0530

    Changes

--- src/Component/Body/Body.jsx
+++ src/Component/Body/Body.jsx
@@ -1,33 +1,72 @@
-import "./Styles/Body.css";
-import Movie from "../Movie/Movie";
-import Header from "../Header/Header";
 import { useEffect, useState } from "react";
+import "./Styles/Body.css";
+import axios from "axios";
 function Body() {
-  const [isWidthChange, setWidthChange] = useState(false);
-  const [movies, setMovies] = useState([]);
-  // useEffect(callback , dependencyArray)
-  useEffect(() => {
-    // console.log("Hi>>>");
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((data) => setMovies(data));
-  }, []);
-  // console.log(">>products", movies);
+  const [dogName, setDogChange] = useState("");
+  const [dogImage, setDogURL] = useState("");
+  const [triggerEffect, setTriggerEffect] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
+  // stopwatch
+  const [stopWatch, setStopWatch] = useState(0);
+
+  // useEffect(() => {
+  //   // updation
+  //   if (triggerEffect >= 1) {
+  //     fetch(`https://dog.ceo/api/breed/${dogName}/images/random`)
+  //       .then((response) => response.json())
+  //       .then((res) => {
+  //         if (res.status == "success") {
+  //           setDogURL(res.message);
+  //           setIsLoading(!isLoading);
+  //         } else if (res.status == "error") {
+  //           setIsLoading(!isLoading);
+  //         }
+  //       })
+  //       .catch((err) => console.error(err));
+  //     // return () => {
+  //     //   // clean up the states
+  //     //   // when u are coming to this useEffect next Time
+  //     //   setDogURL("");
+  //     // };
+  //   }
+  // }, [triggerEffect]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStopWatch((stopWatch) => stopWatch + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [triggerEffect]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsLoading(!isLoading);
+    setTriggerEffect(triggerEffect + 1);
+  };
+  const handleDogName = (event) => {
+    setDogChange(event.target.value);
+  };
+  // console.log(">>dogname", dogName);
   return (
     <div>
-      <Header setWidthChange={setWidthChange} isWidthChange={isWidthChange} />
-      <Movie isWidthChange={isWidthChange} />
-      {movies?.map((item) => {
-        return (
-          <div>
-            <h3>{item.title}</h3>
-          </div>
-        );
-      })}
+      <h3>{stopWatch}</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(event) => handleDogName(event)}
+          type="text"
+          placeholder="Search Dog"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {isLoading && (
+        <img src="https://loading.io/assets/mod/spinner/spinner/lg.gif" />
+      )}
+      {dogImage && <img src={dogImage} alt="preview" />}
     </div>
   );
 }
 export default Body;
-
-// falsy values : undefined , false, 0
